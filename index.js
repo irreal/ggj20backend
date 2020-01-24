07de2e5a6acf364b131444be523c7a0a4c370177
Miloš Spasojević <milos.s.pfc@gmail.com>
2020-01-24 22:39:56 +0100

    Add cors plugin
    Cors plugin enables cross domain communication from web apps with this
    api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 express.json();
+var cors = require('cors');
 const app = express()
+app.use(cors());
 const port = 3000
 
 app.get('/', (req, res) => res.send({ message: "hello svete" }))

6ca561fdf47444d7f43a5af154210b5c177db45c
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2020-01-31 21:42:21 +0100

    Add long polling support

--- index.js
+++ index.js
@@ -8,25 +8,32 @@ app.use(cors());
 app.use(bodyParser.json());
 const port = process.env.PORT || 80;
 
-let eventQueue = [];
+var longpollWithDebug = require("express-longpoll")(app, { DEBUG: true });
+
+// let eventQueue = [];
+
+longpollWithDebug.create('/event')
 
 app.get('/', (req, res) => res.send({ message: `service appears to be up and running :) Version: ${pjson.version}` }))
 app.post('/event', (req, res) => {
     console.log('evo eventa!', req.body);
     if (req.body.action) {
-        eventQueue.push(req.body);
+        // eventQueue.push(req.body);
+        longpollWithDebug.publish('/event', req.body);
     }
     res.sendStatus(201);
 });
-app.get('/event', (req, res) => {
-    if (eventQueue.length === 0) {
-        res.send({});
-    }
-    else {
-        res.send(eventQueue[0]);
-        eventQueue = eventQueue.slice(1);
-        console.log('nakon slanja queue: ', eventQueue);
-    }
-});
+
+
+// app.get('/event', (req, res) => {
+//     if (eventQueue.length === 0) {
+//         res.send({});
+//     }
+//     else {
+//         res.send(eventQueue[0]);
+//         eventQueue = eventQueue.slice(1);
+//         console.log('nakon slanja queue: ', eventQueue);
+//     }
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
